da8ac63dbc4e11668118577ea45531618130ca01
Pere Giménez <gimenezfebrer@gmail.com>
2023-08-31 15:01:32 +0200

    improvements

--- build_api_docs.sh
+++ build_api_docs.sh
@@ -1,41 +1,40 @@
 #!/bin/zsh
-root_folder="/Users/pere/genie/geniedocssite/content/2.docs/"
+root_folder="$(pwd)"
+output_folder="$(realpath $1)/"
+shift
+
 
 # Function to execute your code
 process_folder() {
-	folder_name="$1"
-	destination_folder="$2"
-	echo "Processing folder: $folder_name"
-	echo "Destination folder: $destination_folder"
-	cd $folder_name
-	julia --project make.jl >>documenter.log
-	find build/ -type f -name "*.md" -exec grep -l 'Missing docstring for' {} \; | xargs -I{} sed -i '' -e 's/.*\(Missing docstring for.*\)Check.*/::alert{type="info"}\r\1\r::/g' {}
-	find build/ -type f -name "*.md" -exec sed -i '' -e '/^!!! warning "Missing docstring\."/d' {} \;
-	find $destination_folder/ -type d -name "*.md" -exec rm -r {} +
-	cp -rf build/API/ "$destination_folder/"
-	cd ..
+    docs_folder="$1/docs"
+    output="$2"
+    echo "Docs folder: $docs_folder"
+    echo "Output folder: $output"
+    mkdir -p $output
+    cd $docs_folder
+    julia --project make.jl >>documenter.log 2>&1
+    find build/ -type f -name "*.md" -exec grep -l 'Missing docstring for' {} \; | xargs -I{} sed -i '' -e 's/.*\(Missing docstring for.*\)Check.*/::alert{type="info"}\r\1\r::/g' {}
+    find build/ -type f -name "*.md" -exec sed -i '' -e '/^!!! warning "Missing docstring\."/d' {} \;
+    find $output/ -type d -name "*.md" -exec rm -r {} +
+    cp -rf build/API/* "$output/"
+    cd $root_folder
 }
 
 # Check if arguments are provided
 if [ $# -eq 0 ]; then
-	echo "Usage: $0 folder1 folder2 folder3 ..."
-	exit 1
+    echo "Usage: $0 folder1 folder2 folder3 ..."
+    exit 1
 fi
 
 # Mapping from folder name to destination folder name
-typeset -A folder_map
-folder_map=("genie" "2.server" "stipple" "3.UI" "searchlight" "4.database")
+# typeset -A folder_map
+# folder_map=("genie" "2.server" "stipple" "3.UI" "searchlight" "4.database")
+declare -A folder_map
+folder_map=( ["genie"]="2.server" ["stipple"]="3.UI" ["searchlight"]="4.database" )
 
 # Iterate over the arguments (folder names)
+echo $folder_map
 for folder_name in "$@"; do
-	if [[ -d "$folder_name" ]]; then
-		destination_folder="$root_folder${folder_map[$folder_name]}/99.API/"
-		if [[ -n "$destination_folder" ]]; then
-			process_folder "$folder_name" "$destination_folder"
-		else
-			echo "No mapping found for folder: $folder_name"
-		fi
-	else
-		echo "Folder not found: $folder_name"
-	fi
+    echo "Processing $folder_name"
+    process_folder "packages/$folder_name" "$output_folder${folder_map[$folder_name]}"
 done
